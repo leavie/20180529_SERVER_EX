058000b824112fe42a9a081a6161818f31b2e2f5
leavie <gola520xxx@gmail.com>
2018-05-29 18:48:21 +0800

    db_server

--- connfile/mysql_connect.inc.php
+++ connfile/mysql_connect.inc.php
@@ -2,7 +2,7 @@
 <?php
 //資料庫設定
 //資料庫位置
-$db_server = "localhost:3306";
+$db_server = "localhost";
 //資料庫名稱
 $db_name = "nameacrd";
 //資料庫管理者帳號
@@ -11,8 +11,10 @@ $db_user = "root";
 $db_passwd = "ZGrRt5luKn1G";
 
 //對資料庫連線
-if(!@mysql_connect($db_server, $db_user, $db_passwd))
+if(!@mysql_connect($db_server, $db_user, $db_passwd)) {
+        echo "string";
         die("無法對資料庫連線");
+}
 
 //資料庫連線採UTF8
 mysql_query("SET NAMES utf8");
